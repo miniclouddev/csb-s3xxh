e8bdc782c29c77a5301c57634b68234bdcca7fc9
Code Sandbox <hello@codesandbox.io>
2021-07-13 23:50:51 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.cfbfa36da98a39f2801af35d037f0233.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cee66222af6551541ebe868514af2af",
+    "url": "https://miniclouddev.github.io/csb-s3xxh/index.html"
+  },
+  {
+    "revision": "99dd76d17040de4f9e4f",
+    "url": "https://miniclouddev.github.io/csb-s3xxh/static/css/main.cc091d6b.chunk.css"
+  },
+  {
+    "revision": "f3eb6cbc5077dd4041fd",
+    "url": "https://miniclouddev.github.io/csb-s3xxh/static/js/2.4ce255dc.chunk.js"
+  },
+  {
+    "revision": "99dd76d17040de4f9e4f",
+    "url": "https://miniclouddev.github.io/csb-s3xxh/static/js/main.27a620dd.chunk.js"
+  },
+  {
+    "revision": "c1869001e649bd953a4e",
+    "url": "https://miniclouddev.github.io/csb-s3xxh/static/js/runtime~main.2ed057d6.js"
+  }
+]);
